1ab27bfe4cb350281b1740b90e4cd42688fa0e0c
Bhea <bheaisipnobleza@gmail.com>
2023-04-27 09:44:32 +0800

    final output 4th try

--- highest-grade-generator.py
+++ highest-grade-generator.py
@@ -1,7 +1,7 @@
 # Open the file for reading
 with open("students.txt") as main_file:
     # Initialize variable to keep track of the name who got the highest GWA and his/her GWA
-    highest_gwa = 0
+    highest_gwa = 5.00
     student_highest = ""
     # Loop through each line in the file
     for line in main_file:
@@ -11,9 +11,9 @@ with open("students.txt") as main_file:
         # Convert GWA to float
         gwa = float(gwa[1])
         # If highest
-        if gwa > highest_gwa:
+        if gwa < highest_gwa:
             highest_gwa = gwa
             student_highest = name
 
 # Print the output
-print("The student with the highest grade or GWA is " + (str(student_highest)), "with a GWA of " + (str(highest_gwa)), ". Congratulations!")
+print("The student with the highest grade or GWA is " + (str(student_highest)), "with a GWA of " + (str(highest_gwa)),". Congratulations!")
