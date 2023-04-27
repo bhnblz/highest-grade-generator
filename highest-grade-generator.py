004b5bc4802b83249e3fa0f7d97246ee87a7325f
Bhea <bheaisipnobleza@gmail.com>
2023-04-27 10:20:56 +0800

    final output and adding some python modules

--- highest-grade-generator.py
+++ highest-grade-generator.py
@@ -1,3 +1,15 @@
+import pyfiglet
+description = "Program 2: Name and GWA Generator"
+print(pyfiglet.figlet_format(description.center(15)))
+
+import colorama
+from colorama import Fore
+
+introduction = "Hi, here's the output of this program" 
+print(Fore.LIGHTRED_EX + "=" * 92)
+print(Fore.LIGHTRED_EX + (introduction.center(92)))
+print("=" * 92)
+
 # Open the file for reading
 with open("students.txt") as main_file:
     # Initialize variable to keep track of the name who got the highest GWA and his/her GWA
@@ -16,4 +28,4 @@ with open("students.txt") as main_file:
             student_highest = name
 
 # Print the output
-print("The student with the highest grade or GWA is " + (str(student_highest)), "with a GWA of " + (str(highest_gwa)),". Congratulations!")
+print(Fore.LIGHTMAGENTA_EX + "The student with the highest grade or GWA is " + (str(student_highest)), "with a GWA of " + (str(highest_gwa)),". Congratulations!")
