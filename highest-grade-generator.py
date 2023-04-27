d4212283624978b656c17701ae0c85850696b96f
Bhea <bheaisipnobleza@gmail.com>
2023-04-27 09:10:46 +0800

    pseudocode

---
+++ highest-grade-generator.py
@@ -0,0 +1,6 @@
+# Open the file for reading
+    # Loop through line in the file
+        # Split line into student's GWA and names
+        # Convert GWA to float
+        # If highest
+# Print the output
